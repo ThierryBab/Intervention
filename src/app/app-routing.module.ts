bf10994b672f2a3d407a260e05092bace21ffbe4
ThierryBab <thierry.babault.20@hotmail.fr>
2021-03-15 15:43:37 -0400

    FAIT: TP#6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'bienvenue', component:BienvenueComponent},
+  {path:'acceuil', component:AcceuilComponent},
+  {path:'probleme', component:ProblemeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'acceuil', component:AcceuilComponent},
+      {path:'', redirectTo:'acceuil', pathMatch:'full'},
+      {path:'**', redirectTo:'acceuil', pathMatch:'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
